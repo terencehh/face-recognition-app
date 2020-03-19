cb6e03506de6b92aafd5a02693a99a5c9adc25f7
Terence Ng Han How <ngtere@live.com.sg>
2020-03-19 16:36:45 +1100

    Finalized all API Routing + Added bcrypt.js package for password hashing

--- api-server/server.js
+++ api-server/server.js
@@ -1,23 +1,109 @@
+// Express.js for server-side routing
 const express = require("express");
+// bcrypt for hashing passwords
+const bcrypt = require("bcrypt-nodejs");
 const app = express();
+app.use(express.json());
+
+// Database of users
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "2",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: "987",
+      hash: "",
+      email: "john@gmail.com"
+    }
+  ]
+};
+
+// --> Root Route
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+//signin --> POST Request --> Success/Fail
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (
+    email === database.users[0].email &&
+    password === database.users[0].password
+  ) {
+    res.json("Successfully Signed In");
+  } else {
+    res.status(400).json("Error logging in.");
+  }
+});
+
+//register --> POST Request --> {user}
+app.post("/register", (req, res) => {
+  const { email, password, name } = req.body;
+
+  // hash the password for security
+  bcrypt.hash(password, null, null, (err, hash) => {
+    console.log(hash);
+  });
+
+  database.users.push({
+    id: "3",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+//profile/:userId --> GET Request --> {user}
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("No such User");
+  }
+});
+
+//image --> PUT --> user (with updated count)
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("No such User");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server Successfully Started on Port 3000");
 });
-
-app.get("/", (req, res) => {
-  res.send("This is working");
-});
-
-/*
-hi
-
-/ --> Root Route
-
-/signin --> POST Request --> Success/Fail
-/register --> POST Request --> user
-/profile/:userId --> GET Request --> user
-/image --> PUT --> user (with updated count)
-
-
-*/
