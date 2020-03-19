4f9af2da8f8a3b0334d1df1c44a67d0afe323eeb
Terence Ng Han How <ngtere@live.com.sg>
2020-03-19 12:26:25 +1100

    Started working on Node.js Server Side API

---
+++ api-server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server Successfully Started on Port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("This is working");
+});
+
+/*
+
+/ --> Root Route
+
+/signin --> POST Request --> Success/Fail
+/register --> POST Request --> user
+/profile/:userId --> GET Request --> user
+/image --> PUT --> user (with updated count)
+
+
+*/
