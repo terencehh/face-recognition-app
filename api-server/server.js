9aec336a3ecc8c87f2f9fb8f5c4df18e1d9f20bd
Terence Ng Han How <ngtere@live.com.sg>
2020-03-20 00:57:06 +1100

    Small server side response changes

--- api-server/server.js
+++ api-server/server.js
@@ -88,7 +88,7 @@ app.get("/profile/:id", (req, res) => {
 });
 
 //image --> PUT --> user (with updated count)
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
