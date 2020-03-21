cddcb9a8415b37e44c78975bba0edfab87aaf35b
Terence Ng Han How <ngtere@live.com.sg>
2020-03-21 16:34:07 +1100

    build files

---
+++ react-front-end/build/precache-manifest.9ee97714017e27c547a88be0dce517e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de5c8083766281e7606752891f36497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33fdbd853a6aec9d5614",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e2d2bb52cc42ead2c7c2",
+    "url": "/static/css/main.15565bde.chunk.css"
+  },
+  {
+    "revision": "33fdbd853a6aec9d5614",
+    "url": "/static/js/2.22e89d7b.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.22e89d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d2bb52cc42ead2c7c2",
+    "url": "/static/js/main.c0fe3e04.chunk.js"
+  },
+  {
+    "revision": "7eca8589a2ce1220a484",
+    "url": "/static/js/runtime-main.e551132a.js"
+  }
+]);
