6c2a061dc661d3945984ebc487898db330b41b0b
Terence Ng Han How <ngtere@live.com.sg>
2020-03-21 17:35:24 +1100

    Settings

--- api-server/server.js
+++ api-server/server.js
@@ -14,10 +14,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-reticulated-23112",
-    user: "ngter",
-    password: "123",
-    database: "facerecognitionapp"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
